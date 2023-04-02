99b30d04aad7c3396ac662e34a516f85537dfeb6
IgrPhillipe <IgrPhillipe@gmail.com>
2023-04-02 19:11:32 -0300

    feat: add custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from '@/styles/global'
 import { theme } from '@/styles/theme'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyles />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 };
