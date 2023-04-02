94358e4408a4fc882e51af68fc3fad0dd3b71cd9
IgrPhillipe <IgrPhillipe@gmail.com>
2023-04-02 17:58:04 -0300

    feat: add api model types

---
+++ src/domain/category.ts
@@ -0,0 +1,8 @@
+export type Category = {
+  id: number;
+  name: string;
+  created_by: number;
+  updated_by: number;
+  created_at: string;
+  updated_at: string;
+};
---
+++ src/domain/post.ts
@@ -0,0 +1,56 @@
+import { Author, Category } from "@/domain";
+
+export type CreatedBy = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  username: null;
+};
+
+export type CoverFormat = {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: null;
+  size: number;
+  width: number;
+  height: number;
+  provider_metadata: {
+    public_id: string;
+    resource_type: string;
+  };
+};
+
+export type Cover = CoverFormat & {
+  id: number;
+  alternativeText: string;
+  caption: string;
+  previewUrl: null;
+  provider: string;
+  created_by: number;
+  updated_by: number;
+  created_at: string;
+  updated_at: string;
+  formats: {
+    thumbnail: CoverFormat;
+    small: CoverFormat;
+    medium: CoverFormat;
+    large: CoverFormat;
+  };
+};
+
+export type Post = {
+  id: number;
+  title: string;
+  content: string;
+  slug: string;
+  author: Author;
+  category: Category;
+  created_by: CreatedBy;
+  updated_by: CreatedBy;
+  created_at: string;
+  updated_at: string;
+  cover: Cover;
+};
