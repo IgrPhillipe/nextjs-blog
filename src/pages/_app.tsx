1865a2b2ae91ff4c30dad44bda6bbcf0cf240db5
Igor Phillipe <IgrPhillipe@gmail.com>
2023-04-02 17:41:08 -0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from '@/styles/global'
+import { theme } from '@/styles/theme'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
+};
