7825fed46396c4c4db27f2d21429cf4ce1db68a9
IgrPhillipe <IgrPhillipe@gmail.com>
2023-04-07 18:39:59 -0300

    refactor: add onclick in logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,22 @@ import Image from 'next/image';
 import Link from 'next/link';
 import globeImage from '../../../public/globe.svg';
 
-export default function Header(): JSX.Element {
+const Header = (): JSX.Element => {
   return (
-    <header className="flex w-full items-center justify-between p-6 text-lg ">
-      <div className="flex items-center gap-3">
-        <Image
-          style={{ transform: 'rotate(-12deg)' }}
-          width={50}
-          height={50}
-          src={globeImage}
-        />
-        <h1 className="text-3xl font-bold text-ultra-violet">THE BLOG</h1>
-      </div>
+    <header className="flex w-full items-center justify-between px-12 py-6 text-lg">
+      <Link href="/">
+        <a>
+          <div className="flex items-center gap-3">
+            <Image
+              style={{ transform: 'rotate(-12deg)' }}
+              width={50}
+              height={50}
+              src={globeImage}
+            />
+            <h1 className="text-3xl font-bold text-ultra-violet">THE BLOG</h1>
+          </div>
+        </a>
+      </Link>
 
       <div className="flex items-center justify-center">
         <Link href="/">
@@ -32,4 +36,6 @@ export default function Header(): JSX.Element {
       </div>
     </header>
   );
-}
+};
+
+export default Header;
