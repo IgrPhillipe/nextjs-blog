9bb9fdbe003c2b49efa1f7f64d3eb973b153642f
IgrPhillipe <IgrPhillipe@gmail.com>
2023-04-07 22:46:42 -0300

    refactor: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,11 @@ import { Page } from '@/components';
 export default function PageNotFound() {
   return (
     <Page>
-      <h1>404 - Page Not Found</h1>
+      <div className="flex w-full flex-col items-center justify-center">
+        <h1 className="text-lg font-normal text-ultra-violet">
+          Page not found.
+        </h1>
+      </div>
     </Page>
   );
 }
